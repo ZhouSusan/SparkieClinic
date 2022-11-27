551234cb2d47c89469d6fa357cb39898d5bc07a6
ZhouSusan <szhou089@gmail.com>
2022-11-27 03:01:39 -0800

    added isPaid() and getter for amount attribute in Payment.java

--- src/main/java/SparkieClinic/Payment.java
+++ src/main/java/SparkieClinic/Payment.java
@@ -4,9 +4,18 @@ public class Payment {
 
     private double amount;
     private String date;
+    private boolean paid;
 
     public Payment(double amount, String date) {
         this.amount = amount;
         this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public boolean isPaid() {
+        return paid;
     }
 }
